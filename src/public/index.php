cdb509f416da1eeabb414f4ba0fb6f46c8cb6bbf
Stéphane Bisinger <stephane.bisinger@gmail.com>
2009-04-07 16:22:12 +0100

    Added error displaying and default timezone

--- src/public/index.php
+++ src/public/index.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL|E_STRICT);
+ini_set('display_errors', 1);
+date_default_timezone_set('Europe/Rome');
 // public/index.php
 //
 // Step 1: APPLICATION_PATH is a constant pointing to our
