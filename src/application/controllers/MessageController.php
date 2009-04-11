a6188999af6764f11c367df81579caf678f846cc
Stéphane Bisinger <stephane.bisinger@gmail.com>
2009-04-11 19:28:47 +0100

    Started implementation for sending messages

--- src/application/controllers/MessageController.php
+++ src/application/controllers/MessageController.php
@@ -2,5 +2,12 @@
 
 class MessageController extends Zend_Controller_Action {
 	public function indexAction() {
+	  $this->view->form = $this->_getMessageForm();
+	}
+
+	protected function _getMessageForm() {
+	  $form = new CPond_Form_Message;
+	  $form->setAction($this->_helper->url('index'));
+	  return $form;
 	}
 }
---
+++ src/library/CPond/Form/Message.php
@@ -0,0 +1,24 @@
+<?php
+
+class CPond_Form_Message extends Zend_Dojo_Form {
+  public function init() {
+    $this->setMethod('post');
+
+    $db = Zend_Db_Table::getDefaultAdapter();
+    $users = $db->fetchPairs("SELECT username AS id, username FROM User");
+    $options = array_merge(array("Scrivi il nome utente"), $users );
+    $this->addElement('FilteringSelect','username', array(
+							 'label' => 'Destinatario:',
+							 'required' => true,
+							 'autocomplete' => true,
+							 'multiOptions' => $options,
+							 'validators' => array( array('validator' => 'inArray', 'options' => $users) ),
+							 'onFocus' => 'if(dijit.byId("username").attr("value") == "0") dijit.byId("username").attr("displayedValue", "")'
+							 ));
+    $this->addElement('ValidationTextBox', 'subject', array(
+						  'label' => 'Oggetto:',
+						  'required' => true,
+						  'validators' => array(array('validator' => 'StringLength', 'options' => array(0,255)))
+							    ));
+  }
+}
