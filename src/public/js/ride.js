f7c18e700f7c12dee08bc3f91c4d71fcec7b49a2
Stéphane Bisinger <stephane.bisinger@gmail.com>
2009-04-16 17:02:06 +0100

    Fixed a bug in ride.js and change display to inline for menu

--- src/public/js/ride.js
+++ src/public/js/ride.js
@@ -1,38 +1,42 @@
 function fetchTowns(municipality, context) {
-    if( typeof fetchTowns.baseUrl == 'undefined' ) {
-	fetchTowns.baseUrl = depTownStore._jsonFileUrl;
-    }
-    if( context == "dep") {
-	depTownStore._jsonFileUrl = fetchTowns.baseUrl + "/municipality/" + municipality.value;
-	depTownStore.close();
-    } else if( context == "arr" ) {
-	arrTownStore._jsonFileUrl = fetchTowns.baseUrl + "/municipality/" + municipality.value;
-	arrTownStore.close();
-    }
-    dijit.byId(context + "_town").attr("displayedValue", "");
+	if( typeof fetchTowns.baseUrl == 'undefined' ) {
+		if( depTownStore._jsonFileUrl.indexOf("/municipality") != -1 ) {
+			fetchTowns.baseUrl = depTownStore._jsonFileUrl.substr(0, depTownStore._jsonFileUrl.indexOf("/municipality"));
+		} else {
+			fetchTowns.baseUrl = depTownStore._jsonFileUrl;
+		}
+	}
+	if( context == "dep") {
+		depTownStore._jsonFileUrl = fetchTowns.baseUrl + "/municipality/" + municipality.value;
+		depTownStore.close();
+	} else if( context == "arr" ) {
+		arrTownStore._jsonFileUrl = fetchTowns.baseUrl + "/municipality/" + municipality.value;
+		arrTownStore.close();
+	}
+	dijit.byId(context + "_town").attr("displayedValue", "");
 }
 
 function rideDateChange(input) {
-    if(input.value == 1) {
-	dojo.byId("fieldset-ride_date").style.display = "none";
-	dojo.byId("fieldset-ride_days").style.display = "block";
-    } else {
-	dojo.byId("fieldset-ride_date").style.display = "block";
-	dojo.byId("fieldset-ride_days").style.display = "none";
-    }
+	if(input.value == 1) {
+		dojo.byId("fieldset-ride_date").style.display = "none";
+		dojo.byId("fieldset-ride_days").style.display = "block";
+	} else {
+		dojo.byId("fieldset-ride_date").style.display = "block";
+		dojo.byId("fieldset-ride_days").style.display = "none";
+	}
 }
 
 function rideTypeChange(newtype) {
-    if(newtype.value == 'NEED') {
-	dojo.byId("fieldset-ride_spots").style.display = 'none';
-    } else {
-	dojo.byId("fieldset-ride_spots").style.display = 'block';
-    }
+	if(newtype.value == 'NEED') {
+		dojo.byId("fieldset-ride_spots").style.display = 'none';
+	} else {
+		dojo.byId("fieldset-ride_spots").style.display = 'block';
+	}
 }
 
 dojo.addOnLoad(function () {
-	rideDateChange(dojo.query('[name="repeatable"]:checked'));
-    if( dojo.byId("fieldset-ride_spots") ) {
-        dojo.byId("fieldset-ride_spots").style.display = 'none';
-    }
-    })
+		rideDateChange(dojo.query('[name="repeatable"]:checked'));
+		if( dojo.byId("fieldset-ride_spots") ) {
+		dojo.byId("fieldset-ride_spots").style.display = 'none';
+		}
+		})
